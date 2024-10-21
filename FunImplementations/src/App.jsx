d6c25da3eb5dab39a8a3ce902ddb7e15fa77c3ee
WafleraVertical <101136420+WafleraVertical@users.noreply.github.com>
2024-10-21 01:11:47 -0600

    Base del Proyecto
    Apenas se creo la Primera Pagina del Proyecto

---
+++ FunImplementations/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TTS from './Sections/TTS/tts';
+
+function App() {
+  return (
+    <main style={{padding:'1rem'}}>
+      <TTS/>
+    </main>
+  );
+}
+
+export default App;
---
+++ FunImplementations/src/Sections/TTS/tts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './tts.css';
+
+const TTS = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Mi Página Principal</h1>
+      <p>Esta es la página principal de tu aplicación React.</p>
+    </div>
+  );
+};
+
+export default TTS;
